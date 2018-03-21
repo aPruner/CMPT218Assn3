6f34b5f9b3791ca5d55b7ef42d520d1d38cc35f0
Adam <adampruner99@gmail.com>
2018-03-20 19:22:18 -0700

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      response: ''
+    }
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div>
+        <p>
+          {this.state.response}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
